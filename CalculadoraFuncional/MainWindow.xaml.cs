557befafe4b197db1065e160e6da93ec3db1e3d5
Mazurpro <Davidmazur81@gmail.com>
2023-03-11 02:07:23 +0100

    animacion

--- CalculadoraFuncional/MainWindow.xaml.cs
+++ CalculadoraFuncional/MainWindow.xaml.cs
@@ -1,144 +1,194 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace CalculadoraFuncional
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private double lastNumber;
+        private double result;
+        private bool isNewNumber = true;
+        private string currentOperator;
+        private int firstNumber;
+        private readonly int secondNumber;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Number_Click(object sender, RoutedEventArgs e)
         {
-
+            string digit = (string)((Button)sender).Content;
+            if (isNewNumber)
+            {
+                txtResult.Text = digit;
+                isNewNumber = false;
+            }
+            else
+            {
+                txtResult.Text += digit;
+            }
         }
 
-        private void TabItem_SelectionChanged(object sender, RoutedEventArgs e)
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            string op = (string)((Button)sender).Content;
+            if (currentOperator != null)
+            {
+                double newNumber = double.Parse(txtResult.Text);
+                switch (currentOperator)
+                {
+                    case "+":
+                        result += newNumber;
+                        break;
+                    case "-":
+                        result -= newNumber;
+                        break;
+                    case "*":
+                        result *= newNumber;
+                        break;
+                    case "/":
+                        result /= newNumber;
+                        break;
+                }
+                txtResult.Text = result.ToString();
+            }
+            else
+            {
+                result = double.Parse(txtResult.Text);
+            }
+            currentOperator = op;
+            isNewNumber = true;
+        }
+
+        private void Equal_Click(object sender, RoutedEventArgs e)
         {
 
+            double newNumber = double.Parse(txtResult.Text);
+            switch (currentOperator)
+            {
+                case "+":
+                    result += newNumber;
+                    break;
+                case "-":
+                    result -= newNumber;
+                    break;
+                case "*":
+                    result *= newNumber;
+                    break;
+                case "/":
+                    if (secondNumber != 0)
+                    {
+                        result = firstNumber / secondNumber;
+                    }
+                    else
+                    {
+                        MessageBox.Show("No puedes dividir entre 0.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    break;
+
+            }
+            txtResult.Text = result.ToString();
+            currentOperator = null;
+            isNewNumber = true;
         }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            // Realizar animación para los elementos de texto
+            SolidColorBrush originalBrush = (SolidColorBrush)txtResult.Foreground;
+            SolidColorBrush newBrush = new SolidColorBrush(Colors.Red);
+
+            Storyboard storyboard = new Storyboard();
+            DoubleAnimation fadeOutAnimation = new DoubleAnimation()
+            {
+                From = 1.0,
+                To = 0.0,
+                Duration = TimeSpan.FromSeconds(0.5),
+            };
+            Storyboard.SetTargetProperty(fadeOutAnimation, new PropertyPath("(UIElement.Opacity)"));
+            storyboard.Children.Add(fadeOutAnimation);
+
+            ColorAnimation changeColorAnimation = new ColorAnimation()
+            {
+                From = originalBrush.Color,
+                To = newBrush.Color,
+                Duration = TimeSpan.FromSeconds(0.5),
+            };
+            Storyboard.SetTargetProperty(changeColorAnimation, new PropertyPath("(TextBlock.Foreground).(SolidColorBrush.Color)"));
+            storyboard.Children.Add(changeColorAnimation);
+
+            DoubleAnimation fadeInAnimation = new DoubleAnimation()
+            {
+                From = 0.0,
+                To = 1.0,
+                Duration = TimeSpan.FromSeconds(0.5),
+                BeginTime = TimeSpan.FromSeconds(0.5),
+            };
+            Storyboard.SetTargetProperty(fadeInAnimation, new PropertyPath("(UIElement.Opacity)"));
+            storyboard.Children.Add(fadeInAnimation);
+
+            // Reiniciar valores y ejecutar la animación
+            txtResult.Foreground = newBrush;
+            txtResult.Opacity = 1.0;
+            storyboard.Begin(txtResult);
+
+            // Restablecer valores después de la animación
+            storyboard.Completed += (o, args) =>
+            {
+                txtResult.Foreground = originalBrush;
+            };
+
+            // Restablecer valores de calculadora
+            txtResult.Text = "0";
+            currentOperator = null;
+            result = 0;
+            isNewNumber = true;
+        }
+
+
         private void ButtonDecimal_Click(object sender, RoutedEventArgs e)
         {
-            if (!txtResult.Text.Contains("."))
+            if (!txtResult.Text.Contains(","))
             {
-                txtResult.Text += ".";
+                txtResult.Text += ",";
             }
         }
 
         private void ButtonPercentage_Click(object sender, RoutedEventArgs e)
         {
-            double value = double.Parse(txtResult.Text);
-            value /= 100;
-            txtResult.Text = value.ToString();
+            double number = double.Parse(txtResult.Text);
+            txtResult.Text = (number / 100).ToString();
+            isNewNumber = true;
         }
 
         private void ButtonMemPlusMinus_Click(object sender, RoutedEventArgs e)
         {
-            double value = double.Parse(txtResult.Text);
-            value *= -1;
-            txtResult.Text = value.ToString();
-        }
-        private string number = string.Empty;
-        private string operation = string.Empty;
-        private double firstNumber = 0.0;
-        private double secondNumber = 0.0;
-        private double result = 0.0;
-        private bool isDecimal = false;
-
-        private void Number_Click(object sender, RoutedEventArgs e)
-        {
-            string number = (sender as Button).Content.ToString();
-
-            if (number == "." && txtResult.Text.Contains("."))
-            {
-                // No permitir agregar más de una coma
-                return;
-            }
-
-            if (txtResult.Text == "0" && number != ".")
-            {
-                txtResult.Text = number;
-            }
-            else
-            {
-                txtResult.Text += number;
-            }
-        }
-
-        private void Clear_Click(object sender, RoutedEventArgs e)
-        {
-            txtResult.Text = "0";
-        }
-
-        private void Operator_Click(object sender, RoutedEventArgs e)
-        {
-
-            Console.Write(txtResult+"5614161146153");
-            firstNumber = double.Parse(txtResult.Text);
-            operation = (sender as Button).Content.ToString();
-            txtResult.Text = "";
-        }
-
-        private void Equal_Click(object sender, RoutedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(txtResult.Text))
-            {
-                secondNumber = double.Parse(txtResult.Text);
-
-                switch (operation)
-                {
-                    case "+":
-                        result = firstNumber + secondNumber;
-                        break;
-                    case "-":
-                        result = firstNumber - secondNumber;
-                        break;
-                    case "*":
-                        result = firstNumber * secondNumber;
-                        break;
-                    case "/":
-                        if (secondNumber != 0)
-                        {
-                            result = firstNumber / secondNumber;
-                        }
-                        else
-                        {
-                            MessageBox.Show("Cannot divide by zero.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                        }
-                        break;
-                }
-                txtResult.Text = result.ToString();
-                firstNumber = result;
-            }
-        }
-
-        private void Clear_Click(object sender, RoutedEventArgs e)
-        {
-            number = string.Empty;
-            operation = string.Empty;
-            firstNumber = 0.0;
-            secondNumber = 0.0;
-            result = 0.0;
-            txtResult.Text = "0";
+            double number = double.Parse(txtResult.Text);
+            txtResult.Text = (-number).ToString();
         }
     }
+
+    internal class ResultWindow
+    {
+        private double result;
+
+        public ResultWindow(double result)
+        {
+            this.result = result;
+        }
+    }
+
 
 }
